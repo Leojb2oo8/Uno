dd30671747afcb02275ddf99fb5c29361637bb4e
Leojb2oo8 <118618337+Leojb2oo8@users.noreply.github.com>
2025-02-28 11:01:09 +0000

    index start

--- index.js
+++ index.js
@@ -1,1 +1,77 @@
-Rooms
+currentNameNum = 0;
+
+function randomInt(max, min = 1) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+class Room {
+    constructor(
+        initDoors = randomInt(4),
+    ) {
+        this.name = currentNameNum + 1;
+        currentNameNum++;
+    }
+}
+
+startRoom = ["001", "4", ["L", true, new Room.roomlist], ["L", true, new Room.roomlist], ["L", true, new Room.roomlist], ["L", true, new Room.roomlist]];
+dungeon = [startRoom]
+console.log(dungeon)
+
+import { randomInt, born, randomTFWeight} from "../Utils/general.js";
+import { checkTreesForFood, getFood} from "../Utils/tree-util.js";
+import { checkCreaturesForFood } from "../Utils/creature-util.js";
+
+let currentNameNum = 0;
+export class Creature {
+  name = -1;
+  speed = 0;
+  alive = true;
+  food = 0;
+  livedDays = 0;
+  constructor(
+    initSpeed,
+    initHeight,
+    initCarryStrenth = randomInt(3),
+    initLiveDays = randomInt(6, 3),
+    initThief = [randomTFWeight(100,50), randomInt(100)]
+  ) {
+    this.name = currentNameNum + 1;
+    currentNameNum++;
+    this.speed = initSpeed;
+    this.height = initHeight;
+    this.carryStrenth = initCarryStrenth;
+    this.lifeTime = initLiveDays;
+    this.thief = initThief
+  }
+  feed() {
+    if (checkTreesForFood()) {
+      this.food += getFood(this.carryStrenth, this.height);
+    }
+  }
+
+  steal() {
+    if (this.thief[0] == true && this.food == 0){
+      if (checkCreaturesForFood()) {
+        this.food += getFood(this.carryStrenth, this.height);
+      }
+    }
+  }
+
+
+  dieOrReproduce() {
+    this.livedDays++;
+    if (this.lifeTime > this.livedDays) {
+      if (this.food >= 1) {
+        this.food--;
+        if (this.food > 0) {
+          born(this.food, "Creature", this.speed, this.height);
+          this.food = 0;
+        }
+      } else {
+        this.alive = false;
+      }
+    } else {
+      this.alive = false;
+    }
+  }
+}
