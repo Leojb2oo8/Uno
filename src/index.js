938bc5dd9b1671a6b409fadc3931cb9fa4d65fa7
Leojb2oo8 <118618337+Leojb2oo8@users.noreply.github.com>
2025-03-12 13:31:57 +0000

    start of website

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { card_Deck } from "./cardsjs.js";
 let current_Draw_Cards = card_Deck;
 let current_Discarded_Cards = [] ;
 let players = [["01", []], ["02", []], ["03", []], ["04", []]];
+let starting_Player_Index = -1
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
@@ -14,17 +15,25 @@ function startingHand(startingHand_Ammount = 7){
             let player_Card = current_Draw_Cards[card_Index];
             current_Draw_Cards.splice(card_Index, 1);
             current_Discarded_Cards.push(player_Card);
-            console.log(card_Index, player_Card)
             players[x][1].push(player_Card);
         }
     }
 }
 
-function drawCards(draw_Pile, draw_Ammount = 1){
+function startingPlayer(){
+    starting_Player_Index = getRndInteger(1, players.length+1)
+    return starting_Player_Index
+}
+
+function drawCards(draw_Ammount = 1){
 
 }
 
+function playerTurn(){
+    
+}
+
+
 
 startingHand()
-console.log(players[1][1])
-console.log(current_Discarded_Cards.length)
+console.log()
